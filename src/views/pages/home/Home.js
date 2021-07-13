ed7edc0cf2e16a2d913516399796dea1838b955c
joice.ramos <joice_ramos-rsl@hotmail.com>
2021-07-12 22:49:30 -0300

    adicionado imagens

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,10 +1,16 @@
+import homeSectionOne from '../components/sections/homeSectionOne'
+import homeSectionTwo from '../components/sections/homeSectionTwo'
+
+
 let Home = {
   is_private: false,
 
   render: async () => {
       let view = `
           <div>
-            <h1>Home</div>
+            <h1>Olá meu amorzinhoooooooo s2 s2 s2 s2 s2</div>
+            ${homeSectionOne}
+            ${homeSectionTwo}
           </div>
       `;
 
